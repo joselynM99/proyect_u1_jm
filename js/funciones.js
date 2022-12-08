764e17dba12f1725164c0128991de4002882dc85
Joselyn Moncayo <joselynmoncayo1999@gmail.com>
2022-12-08 15:57:26 -0500

    tarea calculadora

--- js/funciones.js
+++ js/funciones.js
@@ -1,15 +1,12 @@
-function dividir(num1,num2){
-    return num1/num2
+function presionar(n) {
+    var resultado = document.getElementById('resultado').value
+    if (resultado != '0' || isNaN(n)) document.getElementById('resultado').value = (resultado + n)
+    else document.getElementById('resultado').value = n
 }
-
-function multiplicar(num1,num2){
-    return num1*num2
+function calcular() {
+    var resultado = eval(document.getElementById('resultado').value)
+    document.getElementById('resultado').value = resultado
 }
-
-function sumar(num1, num2){
-    return num1+num2
-}
-
-function restar(num1, num2) {
-    return num1-num2
+function reset() {
+    document.getElementById('resultado').value = 0
 }
