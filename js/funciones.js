dc69443801b25a27312ef5b328ca36a80244c8d7
joselynM99 <joselynmoncayo1999@gmail.com>
2022-12-07 19:45:35 -0500

    taller9

---
+++ js/funciones.js
@@ -0,0 +1,15 @@
+function dividir(num1,num2){
+    return num1/num2
+}
+
+function multiplicar(num1,num2){
+    return num1*num2
+}
+
+function sumar(num1, num2){
+    return num1+num2
+}
+
+function restar(num1, num2) {
+    return num1-num2
+}
